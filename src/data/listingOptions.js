87d59c5a90a1d9769afe8992f94f721c00dd5da6
Ai Admins <aiadmins@Bals-MacBook-Pro.local>
2025-10-20 22:40:54 +0530

    Added - listing options mock data file

---
+++ src/data/listingOptions.js
@@ -0,0 +1,25 @@
+// src/data/listingOptions.js
+export const SALE_PROPERTY_TYPES = [
+  "Residential",
+  "Land",
+  "Residential Income",
+  "Residential Lease",
+];
+
+export const RENT_PROPERTY_TYPES = [
+  "Residential Lease",
+  "Residential",
+];
+
+export const PROPERTY_SUBTYPES = [
+  "Single Family Residence",
+  "Apartment/Condo",
+  "Townhouse",
+  "Duplex",
+  "Half Duplex",
+  "Manufactured Home",
+  "Manufactured On Land",
+  "Other",
+  "Recreational",
+  "Quadruplex",
+];
